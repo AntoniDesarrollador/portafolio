b1b3146c2df2e10dc35202b39b7544db9e71543c
Antoni Garcia <garciaantoniherrera@gmail.com>
2018-07-29 21:54:33 -0430

    sección últimos proyectos culminada

--- js/script-carousel.js
+++ js/script-carousel.js
@@ -1,30 +1,30 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    stagePadding: 0,
-    autoWidth: false,
 
-    navText: ['<i class="fas fa-arrow-circle-left" title="anterior"></i>', '<i class="fas fa-arrow-circle-right" title="Siguiente"</i>'],
-    resposive: {
-        0: {
-            items:1
-        },
-        500:{
-            items:2,
-            margin:20,
-            statePadding:10
-        },
-        800:{
-            items:3,
-            margin:20,
-            statePadding:10
-        },
-        1000: {
-            items:4,
-            margin:20,
-            statePadding:10
-        }
-                                
-    }
-})
+$('.owl-carousel').owlCarousel({
+            loop: false,
+            margin: 0,
+            nav: true,
+            autoWidth: false,
+            responsiveClass:true,
+            navText: ['<i class="fas fa-arrow-circle-left" title="anterior"></i>', '<i class="fas fa-arrow-circle-right" title="Siguiente"</i>'],
+            responsive: {
+                0: {
+                    items: 1,
+                    nav:true
+                },
+                500: {
+                    items: 2,
+                    margin: 20
+                    
+                },
+                800: {
+                    items: 3,
+                    margin: 20
+                },
+                1000: {
+                    items: 4,
+                    margin: 20
+                }
+
+            }
+        });
+           
